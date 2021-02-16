0fd6eaf93c9607ea1158a2cde59444c77426c664
brbelic <br.belic@gmail.com>
2021-02-16 22:52:53 +0100

    Added completed toggle for todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Todo({ text }) {
-  return <li className="todo-item">{text}</li>;
+function Todo({ text, isCompleted, isCompletedHandler }) {
+  return (
+    <li className={`todo-item ${isCompleted ? "completed" : ""}`}>
+      <div>
+        <span>{text}</span>
+        <button>Delete</button>
+        <input
+          value={isCompleted}
+          onChange={isCompletedHandler}
+          type="checkbox"
+        ></input>
+      </div>
+    </li>
+  );
 }
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Todo from "./Todo";
 
-function TodoList({ todos }) {
+function TodoList({ todos, setTodos }) {
+  const isCompletedHandler = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) {
+          return todo;
+        }
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        };
+      })
+    );
+  };
+
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <Todo text={todo.text} key={todo.id} />
+          <Todo
+            text={todo.text}
+            key={todo.id}
+            isCompleted={todo.isCompleted}
+            isCompletedHandler={() => isCompletedHandler(todo.id)}
+          />
         ))}
       </ul>
     </div>
